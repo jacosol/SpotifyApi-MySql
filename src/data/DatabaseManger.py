93311aaa16d7c16b991ded95a03c07f7d892fe17
jacosol <copopops@gmail.com>
2020-11-01 14:44:28 +0100

    added data source folder

---
+++ src/data/DatabaseManger.py
@@ -0,0 +1,196 @@
+# @Created on  : 28/10/2020 18:55
+# @Author      : Jacopo Solari
+# @Email       : jaco.solari@gmail.com
+# @File        : DatabaseManger.py
+import datetime
+
+import mysql.connector
+from mysql.connector import errorcode
+from mysql.connector.errors import ProgrammingError
+import pandas as pd
+import os
+
+
+class DataBaseManager():
+    """
+    This class can handle all the operations on a database.
+    """
+    def __init__(self, db_name='myspotify', logging_level=0):
+        self.DB_NAME = db_name
+        print(f'Connecting to {self.DB_NAME} database..')
+        try:
+            self.cnx = mysql.connector.connect(user='root', password='Jacopo87',
+                                               host='localhost',
+                                               database='myspotify')
+            self.cursor = self.cnx.cursor()
+            print(f'Connected to {self.DB_NAME}!')
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print(f"Database \"{self.DB_NAME}\" does not exist")
+            else:
+                print(err)
+        self.logging_level = logging_level
+        self.query = ''
+
+    def create_tables(self, tables):
+        """
+        Create a new table in the database
+        :param tables: dict, the keys are the name of the tables and the values contains
+                the actual query strings needed to create the tables.
+        """
+        for table in tables.keys():
+            print(f'Creating {table} table in {self.DB_NAME}..', end='')
+            try:
+                self.cursor.execute(tables[table])
+            except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                    print("\nERROR: Table already exists.")
+                else:
+                    print(err.msg)
+            else:
+                print("Done")
+
+    def drop_table(self, table, confirm=False):
+        """
+        Drops a table from the database to which the DatabaseManager is connected.
+        :param table: str, the name of the table to drop.
+        :param confirm: bool, if True asks for user confirmation of deletion.
+        """
+        if confirm:
+            confirmation = input(f'Are you sure you want to delete the table \'{table}\'?')
+        else:
+            confirmation = 'y'
+        if confirmation == 'yes' or confirmation[0] == 'y':
+            try:
+                self.cursor.execute(f'drop table {table}')
+                print(f'Deleting {table} table from {self.DB_NAME}..')
+            except ProgrammingError:
+                print(f' The table \'{table}\' does not exist')
+            else:
+                return
+
+    def insert_value(self, table, columns, values):
+        """
+        Insert a row into the database.
+        The method, to be versatile, needs to parse lists and tuples arguments into a query string.
+        :param table:  str, name of the table
+        :param columns: tuple, tuple of strings corresponding to the columns to insert data in
+        :param values: tuple, tuple of mixed types containing the values to add
+        """
+        if self.logging_level > 0:
+            print(f'Values: {values}')
+            print(f'Columns: {columns}')
+        columns = self.remove_chars_from_string(str(columns), '\'')  # removing string apostrophe
+        if '(' not in columns:  # handles the case of single element
+            columns = '(' + columns + ')'
+
+        values_types = self.create_tuple_of_values_types(values)
+        if len(values_types) == 1:
+            values_types = self.remove_chars_from_string(
+                str(values_types), to_remove=[',', '\''])  # removing trailing comma
+        else:
+            values_types = self.remove_chars_from_string(str(values_types), to_remove='\'')
+        # execute MySql statement
+        self.cursor.execute(f'insert into {table} ' +
+                            str(columns) +
+                            f' values {values_types}', values)
+
+    def insert_values_from_file(self, table, filepath, primary_key):
+        """
+        Inserts values read from a file into a table.
+        The columns where the values are inserted are the columns that are
+        both in the file (e.g. csv) and the table we want to insert into.
+        :param table: str, name of the table
+        :param filepath: str, path (relative to the project) of the file containing the data to insert
+        :param primary_key: str, name of the primary key in the table. It has to be also in the file.
+        """
+        df = pd.read_csv(filepath)
+        if primary_key not in list(df.columns):
+            print(f'ERROR: the primary key {primary_key} is not a column of the file '
+                  f'from which the data is supposed to be inserted in the table {table}')
+        df = df.drop_duplicates(subset=primary_key)
+        file_columns = df.columns
+        columns = self.get_columns_names(table)
+        columns_in_common = set(columns).intersection(set(file_columns))
+        print(f'Adding values from {filepath.split(os.sep)[-1]} from these columns: \n   {columns_in_common}')
+        df = df[columns_in_common]
+        for i in range(len(df)):
+            values = list(df.loc[i].values)
+            self.insert_value(table=table,
+                              columns=tuple(columns_in_common),
+                              values=values)
+            self.cnx.commit()
+
+    def get_columns_names(self, table):
+        """
+        Get all the column names from a table into a list.
+        :param table: str, table name
+        :return: list, containing all column names from table
+        """
+        self.cursor.execute(f"SHOW columns FROM {table}")
+        return [column[0] for column in db.cursor.fetchall()]
+
+    def select(self, columns):
+        self.query = 'SELECT ' + self.remove_chars_from_string(str(columns), ['\'', '\"'])
+        return self
+
+    def fromm(self, table):
+        self.query = self.query + ' FROM ' + self.remove_chars_from_string(str(table), ['\'', '\"'])
+        return self
+
+    def where(self, condition, args):
+        self.query = self.query + ' WHERE ' + self.remove_chars_from_string(str(condition), ['\'', '\"'])
+        self.args=args
+        return self
+
+    def run_query(self, verbose=True):
+        self.query = self.query + ';'
+        print(self.query)
+        self.cursor.execute(self.query, self.args)
+        if verbose:
+            for col in self.cursor:
+                print(col)
+
+    @staticmethod
+    def create_tuple_of_values_types(values):
+        """
+        This function matches each object type in values to the key of the type_dict
+        :param values: list, list of objects.
+        :return: tuple, containing the identifiers for each type to insert.
+        """
+        type_dict = {str: "%s", int: "%d", float: "%f"}
+        return tuple(type_dict[type(val)] for val in values)
+
+    @staticmethod
+    def remove_chars_from_string(s, to_remove):
+        if type(to_remove) != list:
+            to_remove = [to_remove]
+        for character in to_remove:
+            s = s.replace(character, '')
+        return s
+
+
+
+db = DataBaseManager()
+
+db.select('*')\
+    .fromm('Albums')\
+    .where(condition='release_date = %s', args=(datetime.date(2020,10,22),))\
+    .run_query()
+
+if 1:
+    db.drop_table('Albums')
+    TABLES = {}
+    TABLES['Albums'] = ('create table Albums '
+                        '(album_id CHAR(22) PRIMARY KEY,'
+                        'name VARCHAR(200), '
+                        'total_track INT,'
+                        'album_type VARCHAR(10),'
+                        'release_date DATE);')
+    db.cnx.commit()
+
+    db.create_tables(TABLES)
+    db.insert_values_from_file(table='Albums', filepath='../../data/trial.csv', primary_key='album_id')
+    db.cnx.commit()
---
+++ src/data/SpotifyAPI.py
@@ -0,0 +1,221 @@
+# @Created on  : 28/10/2020 01:20
+# @Author      : Jacopo Solari
+# @Email       : jaco.solari@gmail.com
+# @File        : SpotifyAPI.py
+
+import os
+import spotipy
+import json
+import pandas as pd
+from spotipy.oauth2 import SpotifyClientCredentials
+import mysql
+
+
+class SpotifyAPI(object):
+    """This class can download data from Spotify API.
+    """
+    def __init__(self):
+        self.authenticate()
+
+    def authenticate(self):
+        my_id = os.environ.get('CLIENT_ID')
+        secret_key = os.environ.get('SECRET_KEY')
+        client_credentials_manager = SpotifyClientCredentials(client_id=my_id, client_secret=secret_key)
+        self.sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    def list_all_genres(self):
+        pass
+
+    def get_newly_released_albums(self, max_number_of_albums=20000, limit=10):
+        response = {'name':[], 'artist':[], 'artist_spotify_link':[], 'artist_spotify_id':[],
+                    'release_date':[], 'album_id':[], 'album_type':[], 'total_tracks':[]}
+        for offset in range(0, max_number_of_albums, limit):
+            r = self.sp.search(q='tag:new', type='album', offset=offset, limit=limit)
+            response['name'].extend([i['name'] for i in r['albums']['items']])
+            response['release_date'].extend([i['release_date'] for i in r['albums']['items']])
+            response['album_type'].extend([i['type'] for i in r['albums']['items']])
+            response['album_id'].extend([i['id'] for i in r['albums']['items']])
+            response['total_tracks'].extend([i['total_tracks'] for i in r['albums']['items']])
+            response['artist'].extend([[i['artists'][j]['name']
+                                        for j in range(len(i['artists']))] for i in r['albums']['items']])
+            response['artist_spotify_id'].extend([[i['artists'][j]['id'] for j in range(len(i['artists']))]
+                                                  for i in r['albums']['items']])
+            response['artist_spotify_link'].extend([[i['artists'][j]['href'] for j in range(len(i['artists']))]
+                                                    for i in r['albums']['items']])
+
+        responsedf = pd.DataFrame(response)
+        responsedf = responsedf.drop_duplicates(subset=['album_id'])
+        print(os.environ.get('IDE_PROJECT_ROOTS'))
+        responsedf.to_csv('../../data/trial.csv')
+        # print(json.dumps(response, indent=1)) # prints nicely a dict
+        print(responsedf['album_id'])
+
+
+    def get_featured_new_releases(self):
+        for j in range(4):
+            response = self.sp.new_releases(limit=50, offset=0, country='IT')
+            while response:
+                albums = response['albums']
+                print(len(albums))
+                for i, item in enumerate(albums['items']):
+                    print(albums['offset'] + i, item['name'], [a['name'] for a in item['artists']])
+
+                if albums['next']:
+                    response = self.sp.next(albums)
+                else:
+                    response = None
+
+    def artist_genre(self, genre):
+        """Download all artists info from a genre """
+
+        artist=[]
+        at=self.sp.search(q='genre:' + genre,limit=50, type='artist')['artists']
+
+        if at['total']>50:
+            for i in range(0,at['total'],50):
+                artist+=self.sp.search(q='genre:'+ genre,limit=50,type='artist',offset=i)['artists']['items']
+        save_json("../../data/artists/{}_artists_info".format(genre),artist)
+        print("Download {}'s artists info".format(genre))
+
+        return artist
+
+
+    def album_artists(self):
+        """Download all albums info from artists """
+        artist=open_json("data/artists/{}_artists_info".format(self.genre))
+        album={}
+
+        for i in artist:
+            name=i['name']
+            album[name]=[]
+            ab=self.sp.artist_albums(i['id'], limit=50)
+
+            if ab['total']>50:
+                for j in range(0,ab['total'],50):
+                    album[name]+=self.sp.artist_albums(i['id'], limit=50,offset=j)['items']
+            else:
+                album[name]+=self.sp.artist_albums(i['id'], limit=50)['items']
+            print("Download {}'s albums info".format(name))
+        save_json("data/albums/{}_albums_info".format(self.genre),album)
+
+        return album
+
+
+    def songs_albums(self):
+        """Download all songs info from albums """
+        album=open_json("data/albums/{}_albums_info".format(self.genre))
+
+        songs={}
+
+        for k,v in album.items():
+            list_dir=os.listdir('./data/tracks/')
+            k=k.replace('/','_')
+
+            if "{}_tracks_info.json".format(k) not in list_dir:
+                songs['artist']=k
+                songs['albums']={}
+
+                for i in v:
+                    songs['albums'][i['id']]=self.sp.album_tracks(i['id'])['items']
+                save_json("data/tracks/{}_tracks_info".format(k),songs)
+                print("Download {}'s tracks info".format(k))
+
+        return songs
+
+
+    def songs_albums_add_popularity(self):
+        list_dir=os.listdir('./data/tracks/')
+
+        for fl in list_dir:
+            try:
+                album=open_json("data/tracks/{}".format(fl[:-5]))
+            except:
+                pass
+            for ak,av in album['albums'].items():
+                for tr in av:
+                    try:
+                        tr['popularity']=self.sp.track(tr['id'])['popularity']
+                    except Exception as e:
+                        print(e,album['artist'])
+            print("Add track popularity to {}'s songs".format(album['artist']))
+            save_json("data/tracks/{}".format(fl[:-5]),album)
+
+
+    def audio_feature(self,artist):
+        """Download audio features of all tracks of one artist
+
+        :artist(string)
+
+        """
+
+        albums=open_json('data/tracks/{}_tracks_info'.format(artist))['albums']
+        Analysis=pd.DataFrame()
+        for k,v in albums.items():
+            audios=[]
+            track_name=[]
+            for i in v:
+                audios.append(i['id'])
+                track_name.append(i['name'])
+            analysis=pd.DataFrame(self.sp.audio_features(audios))
+
+            analysis['track_name']=track_name
+            analysis['album']=self.sp.album(k)['name']
+            analysis['release_date']=self.sp.album(k)['release_date']
+            analysis['album_id']=k
+            analysis['artist']=artist
+            Analysis=Analysis.append(analysis)
+
+        Analysis=Analysis.drop_duplicates('id')
+        Analysis=Analysis[~Analysis.analysis_url.isnull()]
+
+        if 0 in Analysis.columns:
+            del Analysis[0]
+
+        # drop similar songs, highly possible the same songs
+        Analysis=Analysis.drop_duplicates(['acousticness','liveness',
+                                           'instrumentalness','speechiness',
+                                           'danceability','energy','valence'])
+
+        print("Extract tracks analysis of artist {}".format(artist))
+
+        Analysis.to_hdf("./data/analysis/{}_tracks_analysis.h5".format(artist),'analysis')
+
+    def audio_feature_all(self):
+        """Download audio features of all tracks of all artists of one genre
+
+        """
+        dir_l=os.listdir('./data/tracks/')
+        for artist in dir_l:
+            if artist[:-17]+'_tracks_analysis.h5' not in os.listdir('./data/analysis/'):
+                try:
+                    self.audio_feature(artist[:-17])
+                except Exception as e:
+                    print("Artist {} has something wrong:{}".format(artist,e))
+
+def update_data(genre='k-pop'):
+    spf=SpotifyAPI(genre)
+    #upate artists info
+    artist=spf.artist_genre()
+    #update albums info of artists
+    album=spf.album_artists(artist)
+    #update songs info of albums
+    spf.songs_albums(album)
+
+
+def save_json(name,dt):
+    with open('./%s.json'%name,'w') as f:
+        json.dump(dt,f)
+
+
+def open_json(name):
+    with open('./%s.json'%name,'r') as f:
+        dt=json.load(f)
+    return dt
+
+
+if __name__=="__main__":
+
+    #spotf=MySpotify()
+    #spotf.audio_feature_all()
+
+    pass
