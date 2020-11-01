5ca37b2d6151db81a1206d74b532d1a25a0f8b5d
jacosol <copopops@gmail.com>
2020-11-01 13:32:07 +0100

    first commit
    things are getting interesting and thus complex

---
+++ src/scraping/scrape.py
@@ -0,0 +1,7 @@
+# @Created on  : 31/10/2020 11:18
+# @Author      : Jacopo Solari
+# @Email       : jaco.solari@gmail.com
+# @File        : scrape.py
+
+from selenium import webdriver
+
---
+++ src/trials.py
@@ -0,0 +1,5 @@
+# @Created on  : 28/10/2020 01:33
+# @Author      : Jacopo Solari
+# @Email       : jaco.solari@gmail.com
+# @File        : trials.py
+
